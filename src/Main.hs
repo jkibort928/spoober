a47a27926ff4318dcd23436eb0f55dc80f5427ee
Jack Kibort <jkibort928@gmail.com>
2024-04-08 14:47:48 -0700

    working cat

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,19 @@
+import System.IO
+import System.Environment (getArgs)
+import           Control.Exception
+import           Data.Typeable
+newtype Error = Error {errMsg :: String}
+    deriving (Show, Typeable)
+instance Exception Error
+
+
+parseArgs :: [String] -> (String, [String])
+parseArgs (str:strs)  = (str, strs)
+parseArgs []          = throw (Error "Error: No infile specified")
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = do
+    args <- getArgs
+    let (filePath, argv) = parseArgs args
+    rawFile <- readFile filePath
+    putStrLn rawFile
